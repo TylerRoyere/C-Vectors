cf6f01f9c728c475874fbc3c7f95d44400ef310a
Tyler Royere <teroyere@gmail.com>
2021-10-06 23:30:27 -0400

    Name changes and simplification

--- vectors.h
+++ vectors.h
@@ -8,16 +8,17 @@
 
 #include "short_string.h"
 
-#define VEC_TYPE_uint64_t   u64_vector
-#define VEC_TYPE_int64_t    i64_vector
-#define VEC_TYPE_uint32_t   u32_vector
-#define VEC_TYPE_int32_t    i32_vector
-#define VEC_TYPE_void_ptr   str_vector
-#define VEC_TYPE_char_ptr   ptr_vector
-#define VEC_TYPE_short_string   ss_vector
+#define VEC_TYPE_uint64_t   u64v
+#define VEC_TYPE_int64_t    i64v
+#define VEC_TYPE_uint32_t   u32v
+#define VEC_TYPE_int32_t    i32v
+#define VEC_TYPE_void_ptr   strv
+#define VEC_TYPE_char_ptr   ptrv
+#define VEC_TYPE_short_string   short_stringv
+#define VEC_TYPE_i32v i32vv
 
 #define VEC_STRUCT_NAME(T) VEC_TYPE_##T
-#define vec_of(T) struct VEC_STRUCT_NAME(T)
+#define vec_of(T) VEC_STRUCT_NAME(T)
 #define vec_of_ptr(T) vec_of(T##_ptr)
 #define vec_of_struct(T) vec_of(struct_##T)
 #define vec_of_struct_ptr(T) vec_of_ptr(struct_##T)
@@ -47,42 +48,33 @@ GENERATE_VECTOR_FOR_TYPE_PTR(char, true)
 GENERATE_VECTOR_FOR_TYPE_PTR(void, true)
 
 GENERATE_VECTOR_FOR_TYPE(short_string, true)
+GENERATE_VECTOR_FOR_TYPE(i32v, true)
 
-#define VECTOR_GENERIC_ASSOC(pre, x, post) \
-    struct x : pre##x##post \
-
-#define VECTOR_GENERIC(op, vec) VECTOR_GENERIC_ASSOC(, vec, op)
-#define VECTOR_MANAGER(op, vec, post) VECTOR_GENERIC_ASSOC(op, vec, post)
+#define VECTOR_GENERIC(pre, x, post) \
+    x : pre##x##post \
 
 #define TYPE_TO_VECTOR(T, vec, post) \
     T : create_##vec##post
        
-#define VECTOR_GENERICS(op) \
-    VECTOR_GENERIC(op, u64_vector), \
-    VECTOR_GENERIC(op, i64_vector), \
-    VECTOR_GENERIC(op, u32_vector), \
-    VECTOR_GENERIC(op, i32_vector), \
-    VECTOR_GENERIC(op, str_vector), \
-    VECTOR_GENERIC(op, ptr_vector), \
-    VECTOR_GENERIC(op, ss_vector)
-
-#define VECTOR_MANAGERS(op, post) \
-    VECTOR_MANAGER(op, u64_vector, post), \
-    VECTOR_MANAGER(op, i64_vector, post), \
-    VECTOR_MANAGER(op, u32_vector, post), \
-    VECTOR_MANAGER(op, i32_vector, post), \
-    VECTOR_MANAGER(op, str_vector, post), \
-    VECTOR_MANAGER(op, ptr_vector, post), \
-    VECTOR_MANAGER(op, ss_vector, post)
+#define VECTOR_GENERICS(a, b) \
+    VECTOR_GENERIC(a, u64v, b), \
+    VECTOR_GENERIC(a, i64v, b), \
+    VECTOR_GENERIC(a, u32v, b), \
+    VECTOR_GENERIC(a, i32v, b), \
+    VECTOR_GENERIC(a, strv, b), \
+    VECTOR_GENERIC(a, ptrv, b), \
+    VECTOR_GENERIC(a, short_stringv, b), \
+    VECTOR_GENERIC(a, i32vv, b)
 
 #define TYPE_TO_VECTORS(post) \
-    TYPE_TO_VECTOR(uint64_t, u64_vector, post), \
-    TYPE_TO_VECTOR(int64_t, i64_vector, post), \
-    TYPE_TO_VECTOR(uint32_t, u32_vector, post), \
-    TYPE_TO_VECTOR(int32_t, i32_vector, post), \
-    TYPE_TO_VECTOR(char*, str_vector, post), \
-    TYPE_TO_VECTOR(void*, ptr_vector, post), \
-    TYPE_TO_VECTOR(struct short_string*, ss_vector, post)
+    TYPE_TO_VECTOR(uint64_t, u64v, post), \
+    TYPE_TO_VECTOR(int64_t, i64v, post), \
+    TYPE_TO_VECTOR(uint32_t, u32v, post), \
+    TYPE_TO_VECTOR(int32_t, i32v, post), \
+    TYPE_TO_VECTOR(char*, strv, post), \
+    TYPE_TO_VECTOR(void*, ptrv, post), \
+    TYPE_TO_VECTOR(short_string*, short_stringv, post), \
+    TYPE_TO_VECTOR(i32v, i32vv, post)
 
 #define create_vec(T, size) _Generic((*(T*)NULL), \
     TYPE_TO_VECTORS() \
@@ -93,59 +85,59 @@ GENERATE_VECTOR_FOR_TYPE(short_string, true)
     ) (size)
 
 #define init_vec(vec, size) _Generic((vec), \
-    VECTOR_MANAGERS(create_,) \
+    VECTOR_GENERICS(create_,) \
     ) (size)
 
 #define init_vec_cleared(vec, size) _Generic((vec), \
-    VECTOR_MANAGERS(create_,_cleared) \
+    VECTOR_GENERICS(create_,_cleared) \
     ) (size)
 
 #define destroy_vec(a) _Generic((a), \
-    VECTOR_MANAGERS(destroy_,) \
+    VECTOR_GENERICS(destroy_,) \
     ) (a)
 
 #define vec_fill_value(a, b) _Generic((a), \
-    VECTOR_GENERICS(_fill_value) \
+    VECTOR_GENERICS(,_fill_value) \
     ) (a, b)
 
 #define vec_fill(a) _Generic((a), \
-    VECTOR_GENERICS(_fill) \
+    VECTOR_GENERICS(,_fill) \
     ) (a)
 
 #define vec_size(a) _Generic((a), \
-    VECTOR_GENERICS(_size) \
+    VECTOR_GENERICS(,_size) \
     ) (a)
 
 #define vec_resize(a, b) _Generic((a), \
-    VECTOR_GENERICS(_resize) \
+    VECTOR_GENERICS(,_resize) \
     ) (a, b)
 
 #define vec_set(a, b, c) _Generic((a), \
-    VECTOR_GENERICS(_set) \
+    VECTOR_GENERICS(,_set) \
     ) (a, b, c)
 
 #define vec_get(a, b, c) _Generic((a), \
-    VECTOR_GENERICS(_get) \
+    VECTOR_GENERICS(,_get) \
     ) (a, b, c)
 
 #define vec_get_ref(a, b, c) _Generic((a), \
-    VECTOR_GENERICS(_get_ref) \
+    VECTOR_GENERICS(,_get_ref) \
     ) (a, b, c)
 
 #define vec_push(a, b) _Generic((a), \
-    VECTOR_GENERICS(_push) \
+    VECTOR_GENERICS(,_push) \
     ) (a, b)
 
 #define vec_pop(a, b) _Generic((a), \
-    VECTOR_GENERICS(_pop) \
+    VECTOR_GENERICS(,_pop) \
     ) (a, b)
 
 #define vec_last(a, b) _Generic((a), \
-    VECTOR_GENERICS(_last) \
+    VECTOR_GENERICS(,_last) \
     ) (a, b)
 
 #define vec_data(a) _Generic((a), \
-    VECTOR_GENERICS(_data) \
+    VECTOR_GENERICS(,_data) \
     ) (a)
 
 #define vec_foreach_copy_anonymous(a, b, counter) \
@@ -164,13 +156,16 @@ GENERATE_VECTOR_FOR_TYPE(short_string, true)
 #define vec_foreach_copy(a, b) \
     vec_foreach_copy_anonymous_indir(a, b, __COUNTER__)
 
+#define vec_foreach_pop(a, b) \
+        while (vec_size(a) && vec_pop(a, &b).vec)
+
 /*
-GENERATE_VECTOR_FUNCTION_PROTOTYPES(u64_vector, uint64_t, true);
-GENERATE_VECTOR_FUNCTION_PROTOTYPES(i64_vector, int64_t, true);
-GENERATE_VECTOR_FUNCTION_PROTOTYPES(u32_vector, uint32_t, true);
-GENERATE_VECTOR_FUNCTION_PROTOTYPES(i32_vector, int32_t, true);
-GENERATE_VECTOR_FUNCTION_PROTOTYPES(string_vector, char*, true);
-GENERATE_VECTOR_FUNCTION_PROTOTYPES(generic_vector, void*, true);
+GENERATE_VECTOR_FUNCTION_PROTOTYPES(u64v, uint64_t, true);
+GENERATE_VECTOR_FUNCTION_PROTOTYPES(i64v, int64_t, true);
+GENERATE_VECTOR_FUNCTION_PROTOTYPES(u32v, uint32_t, true);
+GENERATE_VECTOR_FUNCTION_PROTOTYPES(i32v, int32_t, true);
+GENERATE_VECTOR_FUNCTION_PROTOTYPES(stringv, char*, true);
+GENERATE_VECTOR_FUNCTION_PROTOTYPES(genericv, void*, true);
 */
 
 #undef GENERATE_VECTOR_STRUCTURE
