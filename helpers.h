f6bd6dc8203db699d3d2a00a5ff5790a3c7d7749
Tyler Royere <teroyere@gmail.com>
2021-10-06 23:28:49 -0400

    Needed for name changes

--- helpers.h
+++ helpers.h
@@ -20,78 +20,80 @@
     *ptr 
 
 #define GENERATE_VECTOR_STRUCTURE(name) \
-    struct name { struct vector* vec; }
+    typedef struct { struct vector* vec; } name
 
 
 #define GENERATE_VECTOR_STATIC_FUNCTIONS(name, type, copyable) \
     GENERATE_VECTOR_STATIC_FUNCTIONS_COPYABLE_##copyable(name, type)
 
 #define GENERATE_VECTOR_STATIC_FUNCTIONS_COPYABLE_true(name, type) \
-static inline struct name create_ ## name(const int capacity) \
+static inline name create_ ## name(const int capacity) \
 { \
-    return (struct name ) { \
+    return (name ) { \
         .vec = create_vector(capacity, sizeof( type )), \
     }; \
 } \
-static inline struct name create_ ## name ##_cleared(const int capacity) \
+static inline name create_ ## name ##_cleared(const int capacity) \
 { \
-    return (struct name ) { \
+    return (name ) { \
         .vec = create_vector_cleared(capacity, sizeof( type )), \
     }; \
 } \
-static inline void name##_fill(struct name vv) \
+static inline name name##_fill(name vv) \
 { \
     vector_fill(vv.vec); \
+    return vv; \
 } \
-static inline void name##_fill_value(struct name vv, type fill) \
+static inline name name##_fill_value(name vv, type fill) \
 { \
     vector_fill_value(vv.vec, &fill); \
+    return vv; \
 } \
-static inline int destroy_ ## name (struct name vv) \
+static inline int destroy_ ## name (name vv) \
 { \
     return destroy_vector(vv.vec); \
 } \
-static inline int name##_size(struct name vv) \
+static inline int name##_size(name vv) \
 { \
     return vector_size(vv.vec); \
 } \
-static inline struct name name##_resize(struct name vv, const int new_capacity) \
+static inline name name##_resize(name vv, const int new_capacity) \
 { \
-    return (struct name) { \
+    return (name) { \
         .vec = vector_resize(vv.vec, new_capacity), \
     }; \
 } \
-static inline type name##_get(const struct name vv, const int index, int* good) \
+static inline type name##_get(const name vv, const int index, int* good) \
 { \
     type ret; \
     NULLABLE_ASSIGNMENT(int, good) = vector_get(vv.vec, index, &ret); \
     return ret; \
 } \
-static inline int name##_set(struct name vv, const int index, type val) \
+static inline int name##_set(name vv, const int index, type val) \
 { \
     return vector_set(vv.vec, index, &val); \
 } \
-static inline type* name##_get_ref(struct name vv, const int index, int* good) \
+static inline type* name##_get_ref(name vv, const int index, int* good) \
 { \
     type* ret = NULL; \
     NULLABLE_ASSIGNMENT(int, good) = vector_get_ref(vv.vec, index, (void**)&ret); \
     return ret; \
 } \
-static inline struct name name##_push(struct name vv, type value) \
+static inline name name##_push(name vv, type value) \
 { \
-    return (struct name) { .vec = vector_push(vv.vec, (void*)&value), }; \
+    return (name) { .vec = vector_push(vv.vec, (void*)&value), }; \
 } \
-static inline struct name name##_pop(const struct name vv, type* ret) \
+static inline name name##_pop(const name vv, type* ret) \
 { \
-    return (struct name) { .vec = vector_pop(vv.vec, ret), };  \
+    return (name) { .vec = vector_pop(vv.vec, ret), };  \
 } \
-static inline type* name##_last(struct name vv, int* good) \
+static inline type* name##_last(name vv, int* good) \
 { \
     type* ret = NULL; \
     NULLABLE_ASSIGNMENT(int, good) = vector_get_ref(vv.vec, vector_size(vv.vec)-1, (void**)&ret); \
     return ret; \
 } \
-static inline type* name##_data(struct name vv) \
+static inline type* name##_data(name vv) \
 { \
     return (type*) vector_data(vv.vec); \
 } \
@@ -103,71 +105,73 @@ static inline type* name##_data(struct name vv) \
 
 
 #define GENERATE_VECTOR_FUNCTIONS_COPYABLE_true(name, type) \
-struct name create_ ## name(const int capacity) \
+name create_ ## name(const int capacity) \
 { \
-    return (struct name ) { \
+    return (name ) { \
         .vec = create_vector(capacity, sizeof( type )), \
     }; \
 } \
-struct name create_ ## name ##_cleared(const int capacity) \
+name create_ ## name ##_cleared(const int capacity) \
 { \
-    return (struct name ) { \
+    return (name ) { \
         .vec = create_vector_cleared(capacity, sizeof( type )), \
     }; \
 } \
-void name##_fill(struct name vv) \
+name name##_fill(name vv) \
 { \
     vector_fill(vv.vec); \
+    return vv; \
 } \
-void name##_fill_value(struct name vv, type fill) \
+name name##_fill_value(name vv, type fill) \
 { \
     vector_fill_value(vv.vec, &fill); \
+    return vv; \
 } \
-int destroy_ ## name (struct name vv) \
+int destroy_ ## name (name vv) \
 { \
     return destroy_vector(vv.vec); \
 } \
-int name##_size(struct name vv) \
+int name##_size(name vv) \
 { \
     return vector_size(vv.vec); \
 } \
-struct name name##_resize(struct name vv, const int new_capacity) \
+name name##_resize(name vv, const int new_capacity) \
 { \
-    return (struct name) { \
+    return (name) { \
         .vec = vector_resize(vv.vec, new_capacity), \
     }; \
 } \
-type name##_get(const struct name vv, const int index, int* good) \
+type name##_get(const name vv, const int index, int* good) \
 { \
     type ret; \
     NULLABLE_ASSIGNMENT(int, good) = vector_get(vv.vec, index, &ret); \
     return ret; \
 } \
-int name##_set(struct name vv, const int index, type val) \
+int name##_set(name vv, const int index, type val) \
 { \
     return vector_set(vv.vec, index, &val); \
 } \
-type* name##_get_ref(struct name vv, const int index, int* good) \
+type* name##_get_ref(name vv, const int index, int* good) \
 { \
     type* ret = NULL; \
     NULLABLE_ASSIGNMENT(int, good) = vector_get_ref(vv.vec, index, (void**)&ret); \
     return ret; \
 } \
-struct name name##_push(struct name vv, type value) \
+name name##_push(name vv, type value) \
 { \
-    return (struct name) { .vec = vector_push(vv.vec, (void*)&value), }; \
+    return (name) { .vec = vector_push(vv.vec, (void*)&value), }; \
 } \
-struct name name##_pop(const struct name vv, type* ret) \
+name name##_pop(const name vv, type* ret) \
 { \
-    return (struct name) { .vec = vector_pop(vv.vec, ret), };  \
+    return (name) { .vec = vector_pop(vv.vec, ret), };  \
 } \
- type* name##_last(struct name vv, int* good) \
+ type* name##_last(name vv, int* good) \
 { \
     type* ret = NULL; \
     NULLABLE_ASSIGNMENT(int, good) = vector_get_ref(vv.vec, vector_size(vv.vec)-1, (void**)&ret); \
     return ret; \
 } \
-type* name##_data(struct name vv) \
+type* name##_data(name vv) \
 { \
     return (type*)vector_data(vv.vec); \
 } \
@@ -178,20 +182,20 @@ type* name##_data(struct name vv) \
      GENERATE_VECTOR_FUNCTION_PROTOTYPES_COPYABLE_##copyable (name, type)
 
 #define GENERATE_VECTOR_FUNCTION_PROTOTYPES_COPYABLE_true(name, type) \
-struct name create_ ## name (const int capacity); \
-struct name create_ ## name ## _cleared(const int capacity); \
-void name##_fill(struct name vv); \
-void name##_fill_value(struct name vv, type fill); \
-int destroy_ ## name (struct name vv); \
-int name##_size(struct name vv); \
-struct name name##_resize(struct name vv, const int new_capacity); \
-type name##_get(const struct name vv, const int index, int* good); \
-int name##_set(struct name vv, const int index, type val); \
-type* name##_get_ref(struct name vv, const int index, int* good); \
-struct name name##_push(struct name vv, type value); \
-struct name name##_pop(const struct name vv, type* ret); \
-type* name##_last(struct name vv, int* good); \
-type* name##_data(struct name vv) \
+name create_ ## name (const int capacity); \
+name create_ ## name ## _cleared(const int capacity); \
+name name##_fill(name vv); \
+name name##_fill_value(name vv, type fill); \
+int destroy_ ## name (name vv); \
+int name##_size(name vv); \
+name name##_resize(name vv, const int new_capacity); \
+type name##_get(const name vv, const int index, int* good); \
+int name##_set(name vv, const int index, type val); \
+type* name##_get_ref(name vv, const int index, int* good); \
+name name##_push(name vv, type value); \
+name name##_pop(const name vv, type* ret); \
+type* name##_last(name vv, int* good); \
+type* name##_data(name vv) \
 
 
 //#define GENERATE_VECTOR_FUNCTIONS_COPYABLE_false(type)
